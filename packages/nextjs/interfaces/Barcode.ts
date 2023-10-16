6cdff0e75125964240203b508ee68076651b8122
mpascualach <miguelpacheson@hotmail.es>
2023-10-16 18:20:18 +0200

    really quick barcode-explorer creation

---
+++ packages/nextjs/interfaces/Barcode.ts
@@ -0,0 +1,5 @@
+export default interface Barcode {
+  id: number;
+  productName: string;
+  description: string;
+}
---
+++ packages/nextjs/pages/barcode-explorer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { MetaHeader } from "~~/components/MetaHeader";
+
+const BarcodeExplorer = () => {
+  return (
+    <>
+      <MetaHeader />
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5">
+          <h1 className="text-center mb-8">
+            <span className="block text-4xl font-bold">Barcode Explorer</span>
+          </h1>
+          <br />
+          <p>This page is under construction. Please check back later.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BarcodeExplorer;
